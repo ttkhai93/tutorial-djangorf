98507390effc0bd5dda93317942cb708ee60f67f
justabitcoiner <justabitcoiner@gmail.com>
2024-07-26 23:56:00 +0700

    fix bug display onwer incorrectly

--- drf_tutorial/snippets/serializers.py
+++ drf_tutorial/snippets/serializers.py
@@ -25,10 +25,11 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+
     class Meta:
         model = Snippet
         fields = ["id", "title", "code", "linenos", "language", "style", "owner"]
-        owner = serializers.ReadOnlyField(source="owner.username")
 
 
 class UserSerializer(serializers.ModelSerializer):
