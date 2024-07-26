92219b1ec00c954c4046cd8c80b827e283186c75
justabitcoiner <justabitcoiner@gmail.com>
2024-07-26 14:47:07 +0700

    Tutorial 2

--- drf_tutorial/snippets/views.py
+++ drf_tutorial/snippets/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from rest_framework import status
 
 
 @csrf_exempt
-def snippet_list(request: HttpRequest):
+def snippet_list(request: HttpRequest, format=None):
     if request.method == "GET":
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
@@ -17,16 +17,16 @@ def snippet_list(request: HttpRequest):
         serializer = SnippetSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @csrf_exempt
-def snippet_detail(request: HttpRequest, pk: int):
+def snippet_detail(request: HttpRequest, pk: int, format=None):
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
         serializer = SnippetSerializer(snippet)
@@ -37,7 +37,7 @@ def snippet_detail(request: HttpRequest, pk: int):
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=404)
+        return JsonResponse(serializer.errors, status=status.HTTP_404_NOT_FOUND)
     elif request.method == "DELETE":
         snippet.delete()
-        return HttpResponse(status=204)
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
